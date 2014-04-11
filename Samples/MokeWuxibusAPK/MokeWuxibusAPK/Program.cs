d37a8ed7aa930095cee4ed7e115a5bdb7c5d08a3
Shaojun Young <ysj031@126.com>
2014-04-11 23:20:31 +0800

    Add MockApp
    + MockApp for WuxiBus, this mock app can mock local calculation, and
    send request to web service to retrieve information

---
+++ Samples/MokeWuxibusAPK/MokeWuxibusAPK/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Windows.Forms;
+using MokeWuxibusAPK.Utils;
+
+namespace MokeWuxibusAPK
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string localObject = "http://app.wifiwx.com/bus/api.php?a=query_line&k=" +
+                                 HttpUtility.UrlEncode("11") + "&key=&nonce=" +
+                                 SignatureUtil.GenerateSeqId() + "&secret=640c7088ef7811e2a4e4005056991a1f&version=0.1";
+            String str1 = SignatureUtil.SHA1(SignatureUtil.FormatUrl(localObject));
+            String str2 = localObject.Replace("&secret=640c7088ef7811e2a4e4005056991a1f", "") + "&signature=" + str1;
+            localObject = str2;
+
+            Console.WriteLine(localObject);
+            using (StreamWriter sw = File.CreateText("D:\\localObject.txt"))
+            {
+                sw.Write(localObject);
+            }
+        }
+    }
+}
---
+++ Samples/MokeWuxibusAPK/MokeWuxibusAPK/Utils/SignatureUtil.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MokeWuxibusAPK.Utils
+{
+    class SignatureUtil
+    {
+        public static String RandomString()
+        {
+            const string randomSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random localRandom = new Random();
+            StringBuilder localStringBuffer = new StringBuilder();
+            for (int i = 0; i < 15; i++)
+            {
+                localStringBuffer.Append(randomSource[localRandom.Next(61)]);
+            }
+
+            return localStringBuffer.ToString();
+        }
+
+        public static String FormatUrl(String paramString)
+        {
+            return paramString.Substring(1 + paramString.IndexOf("?")).Replace("&", "").Replace("=", "").ToUpper();
+        }
+
+        public static String GenerateSeqId()
+        {
+            Random localRandom = new Random();
+            String str = localRandom.Next(1000000).ToString();
+
+            while (str.Length < 6)
+            {
+                str = "0" + str;
+            }
+            return str;
+        }
+
+        public static String SHA1(String paramString)
+        {
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            byte[] arrayOfByte = Encoding.UTF8.GetBytes(paramString);
+            //sha.ComputeHash();
+            StringBuilder localStringBuilder = new StringBuilder(arrayOfByte.Length << 1);
+            for (int i = 0; i < arrayOfByte.Length; i++)
+            {
+                localStringBuilder.Append(string.Format("{0:X}", 0xF & arrayOfByte[i] >> 4));
+                localStringBuilder.Append(string.Format("{0:X}", 0xF & arrayOfByte[i]));
+            }
+            return localStringBuilder.ToString();
+        }
+    }
+}
