7370a5fd1d306b52381e297cc10f0b26746abe79
Shaojun Young <ysj031@126.com>
2014-04-13 22:56:21 +0800

    Framework Construction & Simple MVVMLight for WP8.1
    + Serializer
    + Tile
    + MVVMLight integration

---
+++ org.xepher.kazuma/Host/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Host.Model;
+
+namespace Host.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
